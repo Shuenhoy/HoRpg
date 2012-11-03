9d768f5bff0255bf08f0d3b7f0c77b9de8b1b2c5
Shuenhoy <shuenhoy@gmail.com>
2012-11-03 14:33:51 +0800

    event ok from once

--- source/src/Character.lua
+++ source/src/Character.lua
@@ -1,148 +1,178 @@
 local Sprite,Image,Rect=HoGE.Sprite,HoGE.Image,HoGE.Rect
-
+local global_manager=global_manager
 --1下
 --2左
 --3右
 --4上
 class. Game_Character() do
-	function __c:ctor() 
-		self.step=1
-		self.direction=1
-		self.w=4
-		self.h=4
-		self.x=1
-		self.y=1
-		self.real_x=0
-		self.real_y=0
-		self.move_speed=4
-		self.anime_count=0
-	end
-	function __c:get_rect(w,h)
-		return w/self.w*(self.step-1),h/self.h*(self.direction-1)+1,w/self.w,h/self.h 
-	end
-	function __c:up() 
-	self.direction=4
-		if self:passable(self.x,self.y,4) then
-			
-			self.y=self.y-1
-		end
-	end
-	function __c:down() 
-	self.direction=1
-		if self:passable(self.x,self.y,1) then
-			
-			self.y=self.y+1
-		end
-	end
-	function __c:left()
-	self.direction=2
-	if self:passable(self.x,self.y,2) then
-		
-		self.x=self.x-1
-	end
-	end
-	function __c:right() 
-		self.direction=3
-		if self:passable(self.x,self.y,3) then
-		self.x=self.x+1
-		end
-	end
-	function __c:moveto(x,y)
+  function __c:ctor() 
+    self.step=1
+    self.direction=1
+    self.w=4
+    self.h=4
+    self.real_x=0
+    self.real_y=0
+    self.x=1
+    self.y=1
+    self.z=2
+    
+  
+  
+    self.move_speed=4
+    self.anime_count=0
+    
+  end
+  function __c:moveto(x,y)
+    
+    self.x = x % global_manager.map.width
+    self.y = y % global_manager.map.height
+    self.real_x = (self.x-1) * 128
+    self.real_y = (self.y-1) * 128
+    self.prelock_direction = 0
+  end
+  function __c:get_rect(w,h)
+    return w/self.w*(self.step-1),h/self.h*(self.direction-1)+1,w/self.w,h/self.h 
+  end
+  function __c:up() 
+  self.direction=4
+    if self:passable(self.x,self.y,4) then
+      
+      self.y=self.y-1
+    end
+  end
+  function __c:down() 
+  self.direction=1
+    if self:passable(self.x,self.y,1) then
+      
+      self.y=self.y+1
+    end
+  end
+  function __c:left()
+  self.direction=2
+  if self:passable(self.x,self.y,2) then
+    
+    self.x=self.x-1
+  end
+  end
+  function __c:right() 
+    self.direction=3
+    if self:passable(self.x,self.y,3) then
+    self.x=self.x+1
+    end
+  end
+  
+  function __c:moving()
+    if self.real_x~=(self.x-1)*128 or self.real_y~=(self.y-1)*128 then
+      return true
+    end
+  end
+  function __c:update_move()
+    local  distance = 2 ^ (self.move_speed+1)
+    --下
+    if self.direction==1 then
+      self.real_y=math.min(self.real_y+distance,(self.y-1)*128)
+    --左
+    elseif self.direction==2 then
+      self.real_x=math.max(self.real_x-distance,(self.x-1)*128)      
+    --右
+    elseif self.direction==3 then 
+        self.real_x=math.min(self.real_x+distance,(self.x-1)*128)
+    --上
+    elseif self.direction==4 then 
+        self.real_y=math.max(self.real_y-distance,(self.y-1)*128)
+    end
+    self.anime_count=self.anime_count+5
+    --self.real_x=self.real_x+distance
+  end
+  function __c:passable(x,y,d)
+    
+    local next_x = x+(d==3 and 1 or d==2 and -1 or 0)
+    local next_y = y+(d==1 and 1 or d==4 and -1 or 0)
+    
+    return global_manager.map:passable(next_x,next_y,d)
+  end
+  function __c:update()
+  
+    if self:moving() then
+      self:update_move()
+    
+    if self.anime_count>18-(self.move_speed+1)*2 then
+      self.step=(self.step-2)%4+1
+      self.anime_count=0
+    end
+    end
+  end
 
-	end
-	function __c:moving()
-		if self.real_x~=(self.x-1)*128 or self.real_y~=(self.y-1)*128 then
-			return true
-		end
-	end
-	function __c:update_move()
-		local  distance = 2 ^ (self.move_speed+1)
-		--下
-		if self.direction==1 then
-			self.real_y=math.min(self.real_y+distance,(self.y-1)*128)
-		--左
-		elseif self.direction==2 then
-			self.real_x=math.max(self.real_x-distance,(self.x-1)*128)			
-		--右
-		elseif self.direction==3 then 
-				self.real_x=math.min(self.real_x+distance,(self.x-1)*128)
-		--上
-		elseif self.direction==4 then 
-				self.real_y=math.max(self.real_y-distance,(self.y-1)*128)
-		end
-		self.anime_count=self.anime_count+5
-		--self.real_x=self.real_x+distance
-	end
-	function __c:passable(x,y,d)
-		
-		local next_x = x+(d==3 and 1 or d==2 and -1 or 0)
-		local next_y = y+(d==1 and 1 or d==4 and -1 or 0)
-		
-		return global_manager.map:passable(next_x,next_y,d)
-	end
-	function __c:update()
-		if self:moving() then
-			self:update_move()
-		end
-		if self.anime_count>18-(self.move_speed+1)*2 then
-			self.step=(self.step-2)%4+1
-			self.anime_count=0
-		end
-		
-	end
-	function __c:display_pos()
-		return (self.real_x)/4,(self.real_y)/4+16
-	end
-	
+  
 end
 
 
 class. Game_Player(Game_Character) do
-	local CENTER_X = (getIntPart(Game.Width/2) - 16) * 4  
-	local CENTER_Y = (getIntPart(Game.Height/2) - 16) * 4   
-	
+  local CENTER_X = (getIntPart(Game.Width/2) - 16) * 4  
+  local CENTER_Y = (getIntPart(Game.Height/2) - 16) * 4   
+  function __c:ctor()
+    Game_Player.create_super(self)
+    self.file="img/mrk.png"
+   
+  end
 
-	function __c:update()
-		
-		if not self:moving() then
-			
-			if HoGE.KeyDown(38) then
-				self:up()
-			elseif HoGE.KeyDown(37) then
-				self:left()
-			elseif HoGE.KeyDown(40) then
-				self:down()
-			elseif HoGE.KeyDown(39) then
-				self:right()
-			end
-		end
-		local last_real_x=self.real_x
-		local last_real_y=self.real_y
-		Game_Player.superclass.update(self)
-		
-		if self.real_y>last_real_y and self.real_y-global_manager.map.display_y > CENTER_Y then 
-			global_manager.map:scroll_down(self.real_y-last_real_y)
-		end
-		if self.real_y<last_real_y and self.real_y-global_manager.map.display_y < CENTER_Y then 
-			global_manager.map:scroll_up((last_real_y-self.real_y))
-		end
-		
-		if self.real_x>last_real_x and self.real_x-global_manager.map.display_x > CENTER_X then 
-			global_manager.map:scroll_right(self.real_x-last_real_x)
-		end
-		if self.real_x<last_real_x and self.real_x-global_manager.map.display_x < CENTER_X then 
-			global_manager.map:scroll_left((last_real_x-self.real_x))
-		end
-	end
+  function __c:update()
+    
+    if not self:moving() then
+      
+      if HoGE.KeyDown(38) then
+        self:up()
+      elseif HoGE.KeyDown(37) then
+        self:left()
+      elseif HoGE.KeyDown(40) then
+        self:down()
+      elseif HoGE.KeyDown(39) then
+        self:right()
+      end
+      
+      if HoGE.KeyDown(32) then
+        
+      end
+    end
+    local last_real_x=self.real_x
+    local last_real_y=self.real_y
+    Game_Player.superclass.update(self)
+    
+    if self.real_y>last_real_y and self.real_y-global_manager.map.display_y > CENTER_Y then 
+      global_manager.map:scroll_down(self.real_y-last_real_y)
+    end
+    if self.real_y<last_real_y and self.real_y-global_manager.map.display_y < CENTER_Y then 
+      global_manager.map:scroll_up((last_real_y-self.real_y))
+    end
+    
+    if self.real_x>last_real_x and self.real_x-global_manager.map.display_x > CENTER_X then 
+      global_manager.map:scroll_right(self.real_x-last_real_x)
+    end
+    if self.real_x<last_real_x and self.real_x-global_manager.map.display_x < CENTER_X then 
+      global_manager.map:scroll_left((last_real_x-self.real_x))
+    end
+  end
 end
 
-class. Game_NPC(Game_Character) do
-	function __c:ctor(map_id,eventdata)
-		Game_NPC:create_super()
-		self.map_id=map_id
-		self.id=eventdata.id
-		
-	end
+class. Game_Event(Game_Character) do
+  function __c:ctor(map_id,eventdata)
+    Game_Event.create_super(self)
+    
+    --Game_Event.superclass.ctor(self)-- create_super(self)
+    self.map_id=map_id
+    self.id=eventdata.id
+    self.event=eventdata
+    self.file=eventdata.pages[1].graphic
+    self:moveto(unpack(eventdata.pos))
+    
+  end
+  function __c:start()
+    global_manager.map.interpreter:load(loadstring(self.event.pages[1].work))
+  end
+  function __c:update()
+    Game_Event.superclass.update(self)
+    
+  end
 
 end
 
@@ -150,30 +180,53 @@ end
 
 
 
-class. Display_Character() do	
-	function __c:ctor(character)
-		self.sprite=Sprite:new(2)
-		self.image=nil
-		self.character=character
+class. Display_Character() do  
+  function __c:ctor(character)
+    self.z=character.z
+    self.sprite=Sprite:new(ZPosSet.Character)
+    self.image=nil
+    self.character=character
+    
+    self.rect=Rect:new(0,0,0,0)
+    self.sprite.Rect=self.rect
+    self.file=""
+    self.x=self.character.x
+    self.y=self.character.y
+  end
+  function __c:set_image(img)
+    self.image=img
+    self.sprite:Image(img)
+    self.rect:Set(self.character:get_rect(img.Width,img.Height))
+    
+  end
+  function __c:display_pos()
+    return (self.character.real_x)/4,(self.character.real_y)/4+16
+  end
+  function __c:update()
+    if self.x~=self.character.x then
+      self.x=self.character.y
+    end
 
-		self.rect=Rect:new(0,0,0,0)
-		self.sprite.Rect=self.rect
-		self.sprite:Image(self.image)
-	end
-	function __c:set_image(img)
-		self.image=img
-		self.sprite:Image(img)
-		self.rect:Set(self.character:get_rect(img.Width,img.Height))
-		
-	end
-	function __c:update()
-		
-		self.sprite.Rect:Set(self.character:get_rect(self.image.Width,self.image.Height))
-		self.sprite.X,self.sprite.Y=self.character:display_pos()
-		self.sprite.OX=global_manager.map.display_x/4
-		self.sprite.OY=global_manager.map.display_y/4
-		
-	end
-	
+    if self.file~=self.character.file then
+      self.file=self.character.file
+      
+      self:set_image(HoGE.Image:FromFile(self.character.file))
+      
+    end
+    if self.file~="" then
+    self.sprite.Rect:Set(self.character:get_rect(self.image.Width,self.image.Height))
+    self.sprite.X,self.sprite.Y=self:display_pos()
+  
+    self.sprite.OX=global_manager.map.display_x/4
+    self.sprite.OY=global_manager.map.display_y/4
+    end
+    if self.z~=self.character.z or self.y~=self.character.y then 
+      self.z=self.character.z
+      self.y=self.character.y
+      self.sprite:Z(self.z)
+    end
+
+  end
+  
 end
 
--- source/src/Utilities.lua
+++ source/src/Utilities.lua
@@ -28,7 +28,7 @@ local string=string
 -- @return gbk串
 --------------------------------------------------------------------------------------
 function GBK(s)
-	return coding.utoa(coding.u8tou(s))
+  return coding.utoa(coding.u8tou(s))
 end
 --------------------------------------------------------------------------------------
 -- @class function
@@ -77,36 +77,36 @@ end
 
 
 function print_r(root)
-	local cache = {  [root] = "." }
-	local function _dump(t,space,name)
-		local temp = {}
-		for k,v in pairs(t) do
-			local key = tostring(k)
-			if cache[v] then
-				tinsert(temp,"+" .. key .. " {" .. cache[v].."}")
-			elseif type(v) == "table" then
-				local new_key = name .. "." .. key
-				cache[v] = new_key
-				tinsert(temp,"+" .. key .. _dump(v,space .. (next(t,k) and "|" or " " ).. srep(" ",#key),new_key))
-			else
-				tinsert(temp,"+" .. key .. " [" .. tostring(v).."]")
-			end
-		end
-		return tconcat(temp,"\n"..space)
-	end
-	print(_dump(root, "",""))
+  local cache = {  [root] = "." }
+  local function _dump(t,space,name)
+    local temp = {}
+    for k,v in pairs(t) do
+      local key = tostring(k)
+      if cache[v] then
+        tinsert(temp,"+" .. key .. " {" .. cache[v].."}")
+      elseif type(v) == "table" then
+        local new_key = name .. "." .. key
+        cache[v] = new_key
+        tinsert(temp,"+" .. key .. _dump(v,space .. (next(t,k) and "|" or " " ).. srep(" ",#key),new_key))
+      else
+        tinsert(temp,"+" .. key .. " [" .. tostring(v).."]")
+      end
+    end
+    return tconcat(temp,"\n"..space)
+  end
+  print(_dump(root, "",""))
 end
 function getIntPart(x)
-	if x <= 0 then
-	return math.ceil(x);
-	end
-	
-	if math.ceil(x) == x then
-	x = math.ceil(x);
-	else
-	x = math.ceil(x) - 1;
-	end
-	return x;
+  if x <= 0 then
+  return math.ceil(x);
+  end
+  
+  if math.ceil(x) == x then
+  x = math.ceil(x);
+  else
+  x = math.ceil(x) - 1;
+  end
+  return x;
 end
 
 function Split(str, delim, maxNb)   
@@ -139,39 +139,39 @@ end
 -- @param str
 -- @return number
 function string.utf8len(str)
-	local len = #str
-	local left = len
-	local cnt = 0
-	local arr={0,0xc0,0xe0,0xf0,0xf8,0xfc}
-	while left ~= 0 do
-		local tmp=string.byte(str,-left)
-		local i=#arr
-		while arr[i] do
-			if tmp>=arr[i] then 
-				left=left-i
-				break
-			end
-			i=i-1
-		end
-		cnt=cnt+1
-	end
-	return cnt
+  local len = #str
+  local left = len
+  local cnt = 0
+  local arr={0,0xc0,0xe0,0xf0,0xf8,0xfc}
+  while left ~= 0 do
+    local tmp=string.byte(str,-left)
+    local i=#arr
+    while arr[i] do
+      if tmp>=arr[i] then 
+        left=left-i
+        break
+      end
+      i=i-1
+    end
+    cnt=cnt+1
+  end
+  return cnt
 end
 
 
 
 local function myteadec(input,key)
-	local output_byte={}
-	local length=#input
-	for i=1,length,8 do 
-		local temp=string.sub(input,i,i+7)
-		local out1=teadec(temp,key)
-		table.insert(output_byte,out1)
-	end
-	local out_str=table.concat(output_byte)
-	local n=string.byte(out_str,-1)
+  local output_byte={}
+  local length=#input
+  for i=1,length,8 do 
+    local temp=string.sub(input,i,i+7)
+    local out1=teadec(temp,key)
+    table.insert(output_byte,out1)
+  end
+  local out_str=table.concat(output_byte)
+  local n=string.byte(out_str,-1)
 
-	return string.sub(out_str,1,#out_str-n)
+  return string.sub(out_str,1,#out_str-n)
 end
 
 
@@ -179,30 +179,76 @@ end
 local argv
 local data
 function loader_init(filname,k)
-	argv=arg
-	local uncompress = zlib.inflate()  
-	if argv[2]=="-debug" then
-		local fp=io.open(filname,"rb")
-		require("cjson")
-		
-		
-		if fp==nil then  
-			error [[cannot find the Resource file]];	
-		end
-		
-		
-		data=cjson.decode(myteadec(uncompress(file),k));
-		fp:close()
-	end	
+  argv=arg
+  local uncompress = zlib.inflate()  
+  if arg[2]~="-debug" then
+    local fp=io.open(filname,"rb")
+    require("cjson")
+    
+    
+    if fp==nil then  
+      error [[cannot find the Resource file]];  
+    end
+    
+    
+    data=cjson.decode(myteadec(uncompress(file),k));
+    fp:close()
+  end  
 end
 function load_data(name,t)
-	if argv[2]=="-debug" then
-		local fp=io.open(name,t or "r")
-		local a=fp:read()
-		fp:close()
-		return a
-	else
-		return data[name]
-	end
+  if arg[2]=="-debug" then
+    local fp=assert(io.open(name,t or "r"))
+    local a=fp:read("*a")
+    fp:close()
+    return a
+  else
+    return data[name]
+  end
 end
+function table.foreach(t,f)
+  for k,v in pairs(t) do
+    f(k,v)
+  end
+end
+function table.foreachi(t,f)
+  for i,v in ipairs(t) do
+    f(i,v)
+  end
+end
+
+function samepos(a,b)
+  return (a.x or a.X) == (b.x or b.X) and (a.y or a.Y) == (b.y or b.Y)
+end
+function frontof(a,b,d)
+  if d==4 then
+    return (a.x or a.X) == (b.x or b.X) and (a.y or a.Y) == (b.y or b.Y) - 1
+  elseif d==1 then
+    return (a.x or a.X) == (b.x or b.X) and (a.y or a.Y) == (b.y or b.Y) + 1
+  elseif d==2 then
+    return (a.x or a.X) == (b.x or b.X) - 1 and (a.y or a.Y) == (b.y or b.Y)
+  elseif d==3 then
+    return (a.x or a.X) == (b.x or b.X) + 1 and (a.y or a.Y) == (b.y or b.Y)
+  end
+end
+function frontpos(a,d)
+  if d==4 then
+    return (a.x or a.X) , (a.y or a.Y) - 1
+  elseif d==1 then
+    return (a.x or a.X) , (a.y or a.Y)  + 1
+  elseif d==2 then
+    return (a.x or a.X) - 1 ,(a.y or a.Y) 
+  elseif d==3 then
+    return (a.x or a.X)  + 1 , (a.y or a.Y)
+  end
+end
+
+
+ZPosSet={
+  MapBottom     =   1 ;
+  MapTop        =  10 ; 
+  Character     =   3 ;
+}
+
+
+
 
